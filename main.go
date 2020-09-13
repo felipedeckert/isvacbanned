bf6ca5833cf4ba3d0f00e7dbdaa122a0f085d9b2
Felipe Deckert <fdcosta@uolinc.com>
2020-09-13 19:00:44 -0300

    FDC-01 : param test

--- main.go
+++ main.go
@@ -1,101 +1,23 @@
 package main
 
 import (
-	"bytes"
-	"encoding/json"
-	"errors"
-	"fmt"
 	"isvacbanned/service"
 	"log"
-	"net/http"
-	"os"
-	"strings"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
-// Create a struct that mimics the webhook response body
-// https://core.telegram.org/bots/api#update
-type webhookReqBody struct {
-	Message struct {
-		Text string `json:"text"`
-		Chat struct {
-			ID int64 `json:"id"`
-		} `json:"chat"`
-	} `json:"message"`
-}
-
-// This handler is called everytime telegram sends us a webhook event
-func Handler(res http.ResponseWriter, req *http.Request) {
-	// First, decode the JSON response body
-	body := &webhookReqBody{}
-	if err := json.NewDecoder(req.Body).Decode(body); err != nil {
-		fmt.Println("could not decode request body", err)
-		return
-	}
-
-	// Check if the message contains the word "marco"
-	// if not, return without doing anything
-	if !strings.Contains(strings.ToLower(body.Message.Text), "marco") {
-		return
-	}
-
-	// If the text contains marco, call the `sayPolo` function, which
-	// is defined below
-	if err := sayPolo(body.Message.Chat.ID); err != nil {
-		fmt.Println("error in sending reply:", err)
-		return
-	}
-
-	// log a confirmation message if the message is sent successfully
-	fmt.Println("reply sent")
-}
-
-// Create a struct to conform to the JSON body
-// of the send message request
-// https://core.telegram.org/bots/api#sendmessage
-type sendMessageReqBody struct {
-	ChatID int64  `json:"chat_id"`
-	Text   string `json:"text"`
-}
-
-// sayPolo takes a chatID and sends "polo" to them
-func sayPolo(chatID int64) error {
-	// Create the request body struct
-	reqBody := &sendMessageReqBody{
-		ChatID: chatID,
-		Text:   "Polo!!",
-	}
-	// Create the JSON body from the struct
-	reqBytes, err := json.Marshal(reqBody)
-	if err != nil {
-		return err
-	}
-
-	// Send a post request with your token
-	res, err := http.Post("https://api.telegram.org/bot1324910657:AAFSlJn6TD9EeYNn35MEo-YphYlhYhqc_do/sendMessage", "application/json", bytes.NewBuffer(reqBytes))
-	if err != nil {
-		return err
-	}
-	if res.StatusCode != http.StatusOK {
-		return errors.New("unexpected status" + res.Status)
-	}
-
-	return nil
-}
-
-//func main() {
-//http.ListenAndServe(":3000", http.HandlerFunc(Handler))
-//}
-
 func main() {
 	var (
-		//port      = "3030"                                           //os.Getenv("PORT")
-		//publicURL = "https://is-vac-banned.herokuapp.com/"           //os.Getenv("PUBLIC_URL")
-		//token     = "1324910657:AAFSlJn6TD9EeYNn35MEo-YphYlhYhqc_do" //os.Getenv("TOKEN")
-		port      = os.Getenv("PORT")
-		publicURL = os.Getenv("PUBLIC_URL")
-		token     = os.Getenv("TOKEN")
+		port      = "3030"                                           //os.Getenv("PORT")
+		publicURL = "https://is-vac-banned.herokuapp.com/"           //os.Getenv("PUBLIC_URL")
+		token     = "1324910657:AAFSlJn6TD9EeYNn35MEo-YphYlhYhqc_do" //os.Getenv("TOKEN")
+
+		/*
+			port      = os.Getenv("PORT")
+			publicURL = os.Getenv("PUBLIC_URL")
+			token     = os.Getenv("TOKEN")
+		*/
 	)
 
 	webhook := &tb.Webhook{
@@ -103,21 +25,36 @@ func main() {
 		Endpoint: &tb.WebhookEndpoint{PublicURL: publicURL},
 	}
 
+	setUpBot(webhook, token)
+
+	log.Println("END")
+}
+
+func setUpBot(webhook *tb.Webhook, token string) {
 	pref := tb.Settings{
 		Token:  token,
 		Poller: webhook,
 	}
 
-	b, err := tb.NewBot(pref)
+	bot, err := tb.NewBot(pref)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	b.Handle("/hello", func(m *tb.Message) {
-		b.Send(m.Sender, "Hi!")
+	setUpBotHandlers(bot)
+
+	bot.Start()
+}
+
+func setUpBotHandlers(bot *tb.Bot) {
+	bot.Handle("/hello", func(m *tb.Message) {
+		bot.Send(m.Sender, "Hi!")
 	})
-	b.Start()
-	log.Println("END")
+
+	bot.Handle("/verify", func(m *tb.Message) {
+		bot.Send(m.Sender, m.Payload)
+	})
+
 }
 
 func updatePlayersStatus() {

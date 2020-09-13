95039bdb0e57db0221d3d63f6d5df973e0c589f4
Felipe Deckert <fdcosta@uolinc.com>
2020-09-13 19:08:30 -0300

    FDC-01 : add switch for local or remote bootrun

--- main.go
+++ main.go
@@ -3,22 +3,25 @@ package main
 import (
 	"isvacbanned/service"
 	"log"
+	"os"
 
 	tb "gopkg.in/tucnak/telebot.v2"
 )
 
 func main() {
-	var (
-		port      = "3030"                                           //os.Getenv("PORT")
-		publicURL = "https://is-vac-banned.herokuapp.com/"           //os.Getenv("PUBLIC_URL")
-		token     = "1324910657:AAFSlJn6TD9EeYNn35MEo-YphYlhYhqc_do" //os.Getenv("TOKEN")
 
-		/*
-			port      = os.Getenv("PORT")
-			publicURL = os.Getenv("PUBLIC_URL")
-			token     = os.Getenv("TOKEN")
-		*/
+	var (
+		port      = os.Getenv("PORT")
+		publicURL = os.Getenv("PUBLIC_URL")
+		token     = os.Getenv("TOKEN")
 	)
+
+	local := false
+	if local {
+		port = "3030"                                            //os.Getenv("PORT")
+		publicURL = "https://is-vac-banned.herokuapp.com/"       //os.Getenv("PUBLIC_URL")
+		token = "1324910657:AAFSlJn6TD9EeYNn35MEo-YphYlhYhqc_do" //os.Getenv("TOKEN")
+	}
 
 	webhook := &tb.Webhook{
 		Listen:   ":" + port,

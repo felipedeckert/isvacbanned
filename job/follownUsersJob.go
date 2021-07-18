d8608636a70d699ce7d354d7a6367ca15b84685c
Felipe Deckert <felipe.deckert@wildlifestudios.com>
2021-07-18 20:37:23 -0300

    FDC-05 : adjust  how to measure time elapsed, set temporarily job times to 1 minute

--- job/follownUsersJob.go
+++ job/follownUsersJob.go
@@ -6,6 +6,7 @@ import (
 	"isvacbanned/service"
 	"isvacbanned/util"
 	"log"
+	"sync"
 	"time"
 
 	"github.com/go-co-op/gocron"
@@ -16,42 +17,48 @@ func RunScheduler() {
 	log.Printf("M=RunScheduler\n")
 	scheduler := gocron.NewScheduler(time.UTC)
 
-	scheduler.Every(60).Minutes().Do(checkFollowedUsersBan)
-	scheduler.Every(10).Minutes().Do(checkFollowedUsersNickname)
+	scheduler.Every(1).Minutes().Do(checkFollowedUsersBan)
+	scheduler.Every(1).Minutes().Do(checkFollowedUsersNickname)
 
 	scheduler.StartAsync()
 }
 
 func checkFollowedUsersBan() {
+	log.Print("M=checkFollownUsersBan step=start")
+	currTime := time.Now()
 	usersIncomplete := model.FollowModelClient.GetAllIncompleteFollowedUsers()
-	var usersToComplete []int64
+	var wg sync.WaitGroup
+
 	for chatID, steamIDList := range usersIncomplete {
 		for _, users := range steamIDList {
-			usersToComplete = append(usersToComplete, validateBanStatusAndSendMessage(users, chatID)...)
+			go validateBanStatusAndSendMessage(users, chatID, &wg)
 		}
 	}
-	if len(usersToComplete) > 0 {
-		//Once a player status is set to completed, this player will not be returned in the GetAllIncompleteFollowedUsers query
-		model.FollowModelClient.SetFollowedUserToCompleted(usersToComplete)
-	}
-
-	log.Printf("M=checkFollownUsersBan usersToCompleteCount=%v\n", len(usersToComplete))
+	wg.Wait()
+	elapsedTime := time.Since(currTime)
+	log.Printf("M=checkFollownUsersBan step=end et=%dms", int64(elapsedTime/time.Millisecond))
 }
 
 func checkFollowedUsersNickname() {
+	log.Print("M=checkFollowedUsersNickname step=start")
 	currTime := time.Now()
 	usersIncomplete := model.FollowModelClient.GetAllIncompleteFollowedUsers()
 
+	var wg sync.WaitGroup
+
 	for chatID, steamIDList := range usersIncomplete {
 		for _, users := range steamIDList {
-			go validateNicknameAndSendMessage(users, chatID)
+			wg.Add(1)
+			go validateNicknameAndSendMessage(users, chatID, &wg)
 		}
 	}
+	wg.Wait()
 	elapsedTime := time.Since(currTime)
-	log.Printf("M=checkFollowedUsersNickname et=%dms", int64(elapsedTime / time.Millisecond))
+	log.Printf("M=checkFollowedUsersNickname step=end et=%dms", int64(elapsedTime/time.Millisecond))
 }
 
-func validateBanStatusAndSendMessage(user model.UsersFollowed, chatID int64) []int64 {
+func validateBanStatusAndSendMessage(user model.UsersFollowed, chatID int64, wg *sync.WaitGroup) {
+	defer wg.Done()
 	idsToUpdate := make([]int64, 0)
 	player := service.PlayerServiceClient.GetPlayerStatus(user.SteamID)
 	if len(player.Players) > 0 {
@@ -66,10 +73,15 @@ func validateBanStatusAndSendMessage(user model.UsersFollowed, chatID int64) []i
 			}
 		}
 	}
-	return idsToUpdate
+
+	if len(idsToUpdate) > 0 {
+		//Once a player status is set to completed, this player will not be returned in the GetAllIncompleteFollowedUsers query
+		model.FollowModelClient.SetFollowedUserToCompleted(idsToUpdate)
+	}
 }
 
-func validateNicknameAndSendMessage(user model.UsersFollowed, chatID int64) {
+func validateNicknameAndSendMessage(user model.UsersFollowed, chatID int64, wg *sync.WaitGroup) {
+	defer wg.Done()
 	actualNickname := service.PlayerServiceClient.GetPlayerCurrentNickname(user.SteamID)
 
 	//sometimes the request fails, and it comes empty, hence this validation
--- messenger/messager.go
+++ messenger/messager.go
@@ -30,22 +30,27 @@ var MessengerClient MessengerInterface = Messenger{}
 
 //SendMessage sends a message to user via bot
 func (m Messenger) SendMessage(bot *tb.Bot, user *tb.User, message string) {
+	log.Println("M=SendMessage step=start")
 	_, err := bot.Send(user, message)
 	if err != nil {
 		log.Printf("M=SendMessage err=%s", err.Error())
 	}
+	log.Println("M=SendMessage step=end")
 }
 
 //SendMessageToChat sends a message to chat via bot
 func (m Messenger) SendMessageToChat(bot *tb.Bot, chat *tb.Chat, message string) {
+	log.Println("M=SendMessageToChat step=start")
 	_, err := bot.Send(chat, message)
 	if err != nil {
 		log.Printf("M=SendMessageToChat err=%s", err.Error())
 	}
+	log.Println("M=SendMessageToChat step=end")
 }
 
 //SendMessageToUser sends a message to user/group via telegram API
 func (m Messenger) SendMessageToUser(message string, chatID int64) {
+	log.Println("M=SendMessageToUser step=start")
 	token := os.Getenv("TOKEN")
 
 	sendMessageURL := telegramAPIURL + token + telegramMethod + telegramChatIDParam + strconv.FormatInt(chatID, 10) + telegramTextParam + url.QueryEscape(message)
@@ -54,4 +59,5 @@ func (m Messenger) SendMessageToUser(message string, chatID int64) {
 	if err != nil {
 		log.Printf(`M=SendMessageToUser L=E error sending message URL=%s, err=%s`, sendMessageURL, err.Error())
 	}
+	log.Println("M=SendMessageToUser step=end")
 }
